30a7c4db56358f7cb44ec618e84e70e8bf6d431b
sam-morgan <andersonhenderson45@gmail.com>
2024-02-20 17:41:50 -0500

    Added mock functions and some tests

---
+++ config.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+DB_PATH = (Path(__file__).parent / "server" / "database.db").absolute()
+
---
+++ server/util.py
@@ -0,0 +1,60 @@
+import sqlite3 as sql
+import ../config
+
+def getOrdersForUser(uuid):
+    con = sql.connect(config.DB_NAME)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+
+    try:
+        cur.execute("""
+            SELECT * FROM "Order"
+            WHERE "Order".user = ?
+        """, (uuid,))
+        orders = cur.fetchall()
+
+        return orders
+    except:
+        raise Exception(f"Error occured when trying to retrieve orders for user '{uuid}'.")
+    finally:
+        con.close()
+
+def getUserInfo(uuid):
+    con = sql.connect(config.DB_NAME)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+
+    try:
+        cur.execute("""
+            SELECT * FROM "User"
+            WHERE "User".uuid = ?
+        """, (uuid,))
+        orders = cur.fetchone()
+
+        return orders
+    except:
+        raise Exception(f"Error occured when trying to retrieve info on user '{uuid}'.")
+    finally:
+        con.close()
+
+def getEmailsForUser(uuid):
+    con = sql.connect(config.DB_NAME)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+
+    try:
+        cur.execute("""
+            SELECT "Email"."order", "Email".content, "Email".dateReceived 
+            FROM "Email"
+            JOIN "Order" ON "Email"."order" = "Order".orderID
+            JOIN "User" ON "Order"."user" = "User".uuid
+            WHERE "User".uuid = ?
+        """, (uuid,))
+        emails = cur.fetchall()
+
+        return emails
+    except:
+        raise Exception(f"Error occured when trying to retrieve emails for user '{uuid}'.")
+    finally:
+        con.close()
+
