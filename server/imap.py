d736b27688b256d0ef28a214e6dbeffd673ce7fc
zdeag <deaguiarzachary@gmail.com>
2024-02-21 17:22:01 -0500

    Introduced basic imap functionality

---
+++ server/imap.py
@@ -0,0 +1,100 @@
+import imaplib
+import email
+import time
+
+SERVER = "imap.gmail.com"
+PORT = 993
+EMAIL = "" 
+PASSWORD = ""
+
+SUPPORTED_STORES = [""]
+
+class Email:
+    emailTo = ""
+    emailFrom = ""
+    emailSubject = ""
+    emailDate = ""
+    emailBody = ""
+    trackingCode  = ""
+    orderNumber = ""
+    orderPrice = ""
+
+
+    def __init__(self, emailTo, emailFrom, emailSubject, emailDate, emailBody):
+        self.emailTo = emailTo
+        self.emailFrom = emailFrom
+        self.emailSubject = emailSubject
+        self.emailDate = emailDate
+        self.emailBody = emailBody
+
+# Connect to the mailbox
+def connectMailbox():
+    Mailbox = imaplib.IMAP4_SSL(SERVER, PORT)
+    status = Mailbox.login(EMAIL, PASSWORD)
+
+    if status[0] == 'OK':
+        print("Conected.")
+    else:
+        print("Error.")
+        return None
+
+    Mailbox.select('inbox')
+
+    return Mailbox
+
+# Return a list of emails from the mailbox
+def getEmails(Mailbox):
+    status, data = Mailbox.search(None, 'ALL')
+
+    if status != 'OK':
+        print("No messages found.")
+        return None
+
+    emails = []
+
+    for num in data[0].split():
+        rv, data = Mailbox.fetch(num, '(RFC822)')
+        if rv != 'OK':
+            print("ERROR getting message", num)
+            continue
+
+        msg = email.message_from_bytes(data[0][1])
+        emailTo = msg['To']
+        emailFrom = msg['From']
+        emailSubject = msg['Subject']
+        emailDate = msg['Date']
+        emailBody = msg.get_payload()
+
+        emails.append(Email(emailTo, emailFrom, emailSubject, emailDate, emailBody))
+
+    return emails
+
+def parseEmail(Email):
+    pass
+
+
+
+if __name__ == "__main__":
+
+    Mailbox = connectMailbox()
+    if Mailbox == None:
+        print("Error.")
+        exit()
+
+    while True:
+        emails = getEmails(Mailbox)
+
+        if emails != None:
+            for email in emails:
+                print(email.emailSubject)
+                print(email.emailDate)
+                print(email.emailFrom)
+                print(email.emailTo)
+                print(email.emailBody)
+                print("\n")
+                parseEmail(email)
+        else:
+            print("No emails.")
+            
+        time.sleep(300)
+    
