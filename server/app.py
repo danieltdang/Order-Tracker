7b95b9a7d733bedaea12652ab0fdcf0692df32e8
danieltdang <dtd21b@fsu.edu>
2024-02-22 16:06:55 -0500

    Removed fake data in route

--- server/app.py
+++ server/app.py
@@ -13,40 +13,6 @@ def get_data():
 
 @app.route('/api/getOrders')
 def get_orders():
-    """data = [
-    {
-        "uuid": "5d5408a8-3cb5-4071-afaf-f3154810a140",
-        "id": "1",
-        "name": "Watermelon",
-        "status": 1,
-        "trackingCode": "TRACK123",
-        "estimatedDelivery": datetime.datetime.now(),
-        "carrier": "Carrier 1",
-        "source": "Source 1",
-        "dateAdded": datetime.datetime.now(),
-    },
-    {
-        "uuid": "421926d5-dc62-4e76-b227-3abfa2a1cdd5",
-        "id": "2",
-        "name": "Creatine",
-        "status": 2,
-        "trackingCode": "TRACK246",
-        "estimatedDelivery": datetime.datetime.now(),
-        "carrier": "Carrier 2",
-        "source": "Source 2",
-        "dateAdded": datetime.datetime.now(),
-    },
-    {
-        "uuid": "f3a66b7c-6be1-4e31-becc-9bc04fca382c",
-        "id": "3",
-        "name": "Yeezy 350 V2 Zebra",
-        "status": 0,
-        "trackingCode": "TRACK369",
-        "estimatedDelivery": datetime.datetime.now(),#.strftime("%B %d, %Y %I:%M:%S %p"),
-        "carrier": "Carrier 3",
-        "source": "Source 3",
-        "dateAdded": datetime.datetime.now(),
-    }]"""
     uuid = "12345"
     orders = util.getOrdersForUser(uuid)
     
