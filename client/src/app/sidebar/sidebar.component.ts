a63dd24deb8afadfbe593384f9ef48c0ff720bcc
danieltdang <dtd21b@fsu.edu>
2024-02-22 22:25:59 -0500

    Added temporary sidebar (doesn't scale height)

--- client/src/app/sidebar/sidebar.component.ts
+++ client/src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
+  providers: [MessageService]
 })
 export class SidebarComponent {
-  sidebarVisible: boolean = false;
+  items: MenuItem[] | undefined;
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit() {
+    this.items = [{
+        label: 'Hello',
+        items: [
+          {label: 'Dashboard', icon: 'pi pi-home', routerLink: ['/dashboard']},
+          {label: 'Email Hub', icon: 'pi pi-envelope', routerLink: ['/email-hub']},
+          {label: 'Packages', icon: 'pi pi-download', routerLink: ['/pagename']}, //queryParams: {'recent': 'true'}}
+          {label: 'Settings', icon: 'pi pi-cog', routerLink: ['/settings']},
+          {label: 'FAQ', icon: 'pi pi-question-circle', routerLink: ['/faq']},
+        ]
+    }];
+  }
 }
