e07aa6666bef94507f40ac1bdb9f9a9ad72864db
danieltdang <dtd21b@fsu.edu>
2024-02-24 18:56:12 -0500

    Added subscription to responsively update width size (doesn't update sidebar itself yet)

--- client/src/app/sidebar/ui.service.ts
+++ client/src/app/sidebar/ui.service.ts
@@ -1,12 +1,31 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable, NgZone, OnDestroy } from '@angular/core';
+import { BehaviorSubject, } from 'rxjs';
+import { ViewportRuler } from '@angular/cdk/scrolling';
+
+const MIN_WIDTH = 200;
+const PERCENT_WIDTH = 0.15;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class UiService {
-  private sidebarWidth = new BehaviorSubject<number>(200); // defines the width of the sidebar
+export class UiService implements OnDestroy {
+  private sidebarWidth = new BehaviorSubject<number>(MIN_WIDTH);
   sidebarWidth$ = this.sidebarWidth.asObservable();
+
+  private readonly viewportChange = this.viewportRuler
+    .change(200)
+    .subscribe(() => this.ngZone.run(() => this.setSidebarWidth(Math.max(MIN_WIDTH, this.viewportRuler.getViewportSize().width * PERCENT_WIDTH))));
+  
+  constructor(
+    private readonly viewportRuler: ViewportRuler,
+    private readonly ngZone: NgZone
+  ) {
+    this.setSidebarWidth(Math.max(MIN_WIDTH, this.viewportRuler.getViewportSize().width * PERCENT_WIDTH));
+  }
+
+  ngOnDestroy() {
+    this.viewportChange.unsubscribe();
+  }
 
   setSidebarWidth(width: number) {
     this.sidebarWidth.next(width);
