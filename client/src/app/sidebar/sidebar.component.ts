8b04b64989161451d27fb9d09e1c52158d3cdd8b
danieltdang <dtd21b@fsu.edu>
2024-02-25 01:37:13 -0500

    Made logo redirect to dashboard when clicked

--- client/src/app/sidebar/sidebar.component.ts
+++ client/src/app/sidebar/sidebar.component.ts
@@ -19,7 +19,7 @@ export class SidebarComponent implements OnInit {
         items: [
           {label: 'Dashboard', icon: 'pi pi-home', routerLink: ['/dashboard']},
           {label: 'Email Hub', icon: 'pi pi-envelope', routerLink: ['/email-hub']},
-          {label: 'Packages', icon: 'pi pi-download', routerLink: ['/pagename']}, //queryParams: {'recent': 'true'}}
+          {label: 'Packages', icon: 'pi pi-download', routerLink: ['/packages']}, //queryParams: {'recent': 'true'}}
           {label: 'Settings', icon: 'pi pi-cog', routerLink: ['/settings']},
           {label: 'FAQ', icon: 'pi pi-question-circle', routerLink: ['/faq']},
         ]
