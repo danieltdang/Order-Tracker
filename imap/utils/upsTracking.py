e21972f799215ebfb3e058da76edd1689bf38375
zdeag <deaguiarzachary@gmail.com>
2024-02-28 10:29:28 -0500

    Added trackUPS that returns tracking info as json obj

--- imap/utils/upsTracking.py
+++ imap/utils/upsTracking.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
 import requests
 import os
-import base64
+import uuid
 
+from requests.models import Response
+
+# Returns a UPS API access token
 def getToken():
     load_dotenv()
 
@@ -17,17 +20,14 @@ def getToken():
     url = "https://wwwcie.ups.com/security/v1/oauth/token"
 
     payload = {
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-        "grant_type": "client_credentials"
+       "grant_type": "client_credentials"
     }
 
     headers = {
-    'authorization': 'Basic TTYxN3UxT2pKWnV6ZEFoN1luTFFFY01haG5jYXpBcVczcWdBQWNLTVFvNDlRdWR3OnZTYjVlUDQ2VVphbFA3d3dtdWdPZkhhY2NWcklZRGc4elVmUmJCbmFlcjFuaXQ2NDJJaU5JSDdXR2J5YmZEMXk=',
     'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
     }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload, auth=(CLIENT_ID, CLIENT_SECRET))
 
     try:
         return response.json()["access_token"]
@@ -37,28 +37,21 @@ def getToken():
 def refreshToken():
     pass
 
+# Returns a JSON object with tracking information
 def trackUPS(trackingNumber):
     ACCESS_TOKEN = getToken()
 
     if ACCESS_TOKEN == None:
         return
 
-    url = f"https://wwwcie.ups.com/track/v1/details/{trackingNumber}"
-
-    query = {
-      "locale": "en_US",
-      "returnSignature": "false",
-      "returnMilestones": "false"
-    }
+    url = f"https://wwwcie.ups.com/api/track/v1/details/{trackingNumber}?locale=en_US&returnSignature=false&returnMilestones=false"
 
     headers = {
-      "transId": "string",
+      "transId": str(uuid.uuid4()),
       "transactionSrc": "testing",
       "Authorization": "Bearer " + ACCESS_TOKEN
     }
 
-    response = requests.get(url, headers=headers, params=query)
-
-    print(response.text)
+    response = requests.get(url, headers=headers)
 
     return response.json()
