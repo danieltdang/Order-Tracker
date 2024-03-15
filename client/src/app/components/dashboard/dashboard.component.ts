8034c8a07d9997953997cdf8d2e4374298dd7bea
danieltdang <dtd21b@fsu.edu>
2024-03-14 20:43:52 -0400

    Added Reports section and implemented backend for Reports filters

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent implements OnInit {
   cards: any[] | undefined;
-  
+  chartFilter: any[] | undefined;
+  reportsFilter: any[] | undefined;
+  today = new Date();
+
+  // Function to get the color of the change in the card
   getChangeColor(change: number, title: string): string {
     // Special case for the 'Returned' category
     if (title === 'Returned') {
@@ -28,6 +32,23 @@ export class DashboardComponent implements OnInit {
       return 'text-gray-500';
     }
   }
+
+  getStartDateOfTheWeek = (date: Date) => {
+    const dayOfWeek = date.getDay(); // Get current day of the week, 0 (Sunday) to 6 (Saturday)
+    return new Date(date.setDate(date.getDate() - dayOfWeek));
+  };
+
+  getFirstDayOfMonth = (date: Date) => {
+    return new Date(date.getFullYear(), date.getMonth(), 1);
+  };
+
+  getFirstDayOfTheYear = (date: Date) => {
+    return new Date(date.getFullYear(), 0, 1);
+  };
+
+  formatDate = (date: Date) => {
+    return date.toISOString().split('T')[0]; // Format as 'YYYY-MM-DD'
+  };
 
   ngOnInit() {
     this.cards = [
@@ -70,6 +91,44 @@ export class DashboardComponent implements OnInit {
         icon: 'pi-undo',
         color: 'red',
         change: 1,
+      },
+    ];
+
+    this.reportsFilter = [
+      {
+        name: 'Yesterday',
+        startDate: this.formatDate(new Date(new Date().setDate(new Date().getDate() - 1))),
+        endDate: this.formatDate(this.today),
+      },
+      {
+        name: 'This Week',
+        startDate: this.formatDate(this.getStartDateOfTheWeek(this.today)),
+        endDate: this.formatDate(this.today),
+      },
+      {
+        name: 'This Month',
+        startDate: this.formatDate(this.getFirstDayOfMonth(this.today)),
+        endDate: this.formatDate(this.today),
+      },
+      {
+        name: 'Past 3 Months',
+        startDate: this.formatDate(this.getFirstDayOfMonth(new Date(new Date().setMonth(this.today.getMonth() - 3)))),
+        endDate: this.formatDate(this.today),
+      },
+      {
+        name: 'Past 6 Months',
+        startDate: this.formatDate(this.getFirstDayOfMonth(new Date(new Date().setMonth(this.today.getMonth() - 6)))),
+        endDate: this.formatDate(this.today),
+      },
+      {
+        name: 'This Year',
+        startDate: this.formatDate(this.getFirstDayOfTheYear(this.today)),
+        endDate: this.formatDate(this.today),
+      },
+      { 
+        name: 'All Time',
+        startDate: null,
+        endDate: null
       },
     ];
   }
