67a326c5b9384462101455a6feb3694d70c8a206
sam-morgan <andersonhenderson45@gmail.com>
2024-04-15 22:08:44 -0400

    added email hub content added emails to DB

--- client/src/app/components/email-hub/email-hub.component.ts
+++ client/src/app/components/email-hub/email-hub.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-email-hub',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './email-hub.component.css'
 })
 export class EmailHubComponent {
+  allEmails: any
+  constructor(private apiService: ApiService, private router: Router) {}
 
+  ngOnInit(): void {
+    this.apiService.getUserEmails().subscribe((fetchedData: any) => {
+      this.allEmails = fetchedData.data;
+    });
+  }
+
+  goToOrder(orderID: string) {
+    this.router.navigate(['app/packages', orderID]);
+  }
 }
