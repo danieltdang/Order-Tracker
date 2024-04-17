740d6d71b793c039aa9d1d474b839ddde24add98
danieltdang <dtd21b@fsu.edu>
2024-04-17 13:43:00 -0400

    Added error check if going to package not under user or not existing

--- client/src/app/components/packages/package-detail/package-detail.component.ts
+++ client/src/app/components/packages/package-detail/package-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PackageStatusService } from '../package-status.service';
 import { ApiService } from '../../../services/api.service';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-package-detail',
@@ -12,7 +14,8 @@ export class PackageDetailComponent implements OnInit {
   packageID: string | undefined;
   order : any;
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService, private statusService: PackageStatusService) { }
+  constructor(private route: ActivatedRoute, private apiService: ApiService, private statusService: PackageStatusService,
+              private router: Router, private messageService: MessageService) { }
 
   getStatus(status: number) {
     return this.statusService.getStatus(status);
@@ -25,11 +28,21 @@ export class PackageDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.packageID = params['id'];
-      this.apiService.getOrderByID(params['id']).subscribe((fetchedData: any) => {
-        this.order = fetchedData;
+      this.apiService.getOrderByID(params['id']).subscribe({
+        next: (fetchedData: any) => {
+          this.order = fetchedData;
+        },
+        error: (error) => {
+          if (error.status === 404) {
+            console.log(`Package #${this.packageID} does not exist`)
+            this.router.navigate(['/app/packages']);
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: `Package #${this.packageID} does not exist`, life: 3000 });
+          } else {
+            this.router.navigate(['/app/packages']);
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: `An unexpected error occurred`, life: 3000 });
+          }
+        }
       });
-      
-    })
-    
-  }
+    });
+  }  
 }
