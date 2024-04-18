7b791ce1ee51c88d86850b4fa4b0eb55dc2e92da
danieltdang <dtd21b@fsu.edu>
2024-04-18 01:40:23 -0400

    Added old email viewer logic

--- client/src/app/components/email-hub/email-viewer/email-viewer.component.ts
+++ client/src/app/components/email-hub/email-viewer/email-viewer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Email } from '../../../interfaces/email';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-email-viewer',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './email-viewer.component.css'
 })
 export class EmailViewerComponent {
+  email!: Email;
 
+  constructor(private router: Router) { }
+
+  goToOrder(orderID: string) {
+    this.router.navigate(['app/packages', orderID]);
+  }
 }
